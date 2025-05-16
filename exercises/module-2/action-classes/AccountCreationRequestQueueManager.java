7dcb5952f55bc1b722306ef83372f5d16c18c433
Davi Costa <davi.costa@liferay.com>
2025-05-16 15:05:05 -0300

    Add new action package path and remove Base Reset Controller file

--- exercises/module-2/action-classes/AccountCreationRequestQueueManager.java
+++ exercises/module-2/action-classes/AccountCreationRequestQueueManager.java
@@ -1,4 +1,4 @@
-package com.liferay.clarity;
+package com.clarityvisionsolutions.distributor.mgmt.actions;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
